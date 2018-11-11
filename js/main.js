3ef94fd811419c0a006d3743500bb18402c4c0eb
Dan Spector <danno2423@gmail.com>
2018-11-10 19:11:44 -0500

    rewrote the entire thing to d3.force

--- js/main.js
+++ js/main.js
@@ -31,57 +31,97 @@ d3.csv('../data/food_insecurity.csv', d => {
       });
       console.log(consolidated_data);
 
-      const obj = {name: 'neighborhoods', children:[]}
+      // const obj = {name: 'neighborhoods', children:[]}
 
-      for(i=0; i < consolidated_data.length; i++){
-        obj.children.push({
-          name: consolidated_data[i].neighborhood,
-          children: consolidated_data[i]
-        })
-      }
-      console.log(obj)
+      // for(i=0; i < consolidated_data.length; i++){
+      //   obj.children.push({
+      //     name: consolidated_data[i].neighborhood,
+      //     children: consolidated_data[i]
+      //   })
+      // }
+      // console.log(obj)
 
-      const diameter = 600;
+      // const diameter = 600;
 
       //help from https://bl.ocks.org/alokkshukla/3d6be4be0ef9f6977ec6718b2916d168
+
+      const width = 500;
+      const height = 500;
+
       const svg = d3.select('#svg_container')
       .append('svg')
-      .attr('width', diameter)
-      .attr('height', diameter)
-      .attr('class', 'bubble')
+      .attr('width', width)
+      .attr('height', height)
+      .style('background-color', 'lightgray')
+      .append('g')
+      .attr('transform', 'translate(0,0)')
 
-      
-      const color = d3.scaleSequential(d3.schemeRdYlGn);
-      const pack = d3.pack()
-        .size([diameter, diameter])
-        .padding(1.5);
+      const radiusScale = d3.scaleSqrt().domain([5000, 120000]).range([10,80]);
 
-      const root = d3.hierarchy(obj)
-        .sum(d => d.population)
-      
-      const data_pack = pack(root).descendants(),
-        circlePositions = {};
+      //the simulation is a collection of forces 
+      //about where we want our forcs to go and how
+      //we want our forces to interact
+      //step one: get them to the middle
+      //step two: don't have them collide
+      const simulation = d3.forceSimulation()
+        .force('x', d3.forceX(width / 2).strength(0.05))
+        .force('y', d3.forceY(height / 2).strength(0.05))
+        .force('collide', d3.forceCollide(d => {
+          return radiusScale(d.population) + 1;
+        }))
 
-      const node = svg.selectAll('.node')
-        .data(data_pack)
+      const circles = svg.selectAll('.neighborhoods')
+        .data(consolidated_data)
         .enter()
-        .append('g')
-        .attr('class', d => {
-          console.log(d)
-          circlePositions[d.data.name] = {x: d.x, y:d.y}
-          return d.children ? 'node' : 'leaf node';
-        })
-        .attr('transform', d =>{
-          circlePositions[d.data.name] = {x: d.x, y:d.y}
-          return `translate(${d.x},${d.y})`
-        })
+        .append('circle')
+        .attr('class', 'neighborhood')
+        .attr('r', d => radiusScale(d.population))
+        .attr('fill', 'steelblue')
+        .on('click', d => console.log(d))
 
-      node.append('title')
-        .text(d => {
-          return `${d.neighborhood}:${d.population}`
-        });
+      simulation.nodes(consolidated_data)
+        .on('tick', ticked);
 
-      node.append('circle')
-        .attr('r', d => d.r)
-        .style('fill', d => color(d.population));
+      function ticked() {
+        circles
+          .attr('cx', d => d.x)
+          .attr('cy', d => d.y)
+      }
+      
+
+      
+      // const color = d3.scaleSequential(d3.schemeRdYlGn);
+
+      // const pack = d3.pack()
+      //   .size([diameter, diameter])
+      //   .padding(1.5);
+
+      // const root = d3.hierarchy(obj)
+      //   .sum(d => d.population)
+      
+      // const data_pack = pack(root).descendants(),
+      //   circlePositions = {};
+
+      // const node = svg.selectAll('.node')
+      //   .data(data_pack)
+      //   .enter()
+      //   .append('g')
+      //   .attr('class', d => {
+      //     console.log(d)
+      //     circlePositions[d.data.name] = {x: d.x, y:d.y}
+      //     return d.children ? 'node' : 'leaf node';
+      //   })
+      //   .attr('transform', d =>{
+      //     circlePositions[d.data.name] = {x: d.x, y:d.y}
+      //     return `translate(${d.x},${d.y})`
+      //   })
+
+      // node.append('title')
+      //   .text(d => {
+      //     return `${d.neighborhood}:${d.population}`
+      //   });
+
+      // node.append('circle')
+      //   .attr('r', d => d.r)
+      //   .style('fill', d => color(d.population));
   });
